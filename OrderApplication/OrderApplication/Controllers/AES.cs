ca67d88cc2bfdec31585ec58c9d5c467652b68e6
Johnson Kaho Poon <kahopoon@gmail.com>
2020-02-15 00:49:14 +0800

    AES class for password manipulation before commit into database

---
+++ OrderApplication/OrderApplication/Controllers/AES.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+namespace OrderApplication.Controllers
+
+{
+    public class AES
+    {
+        private static RijndaelManaged rijndael = new RijndaelManaged();
+        private static System.Text.UnicodeEncoding unicodeEncoding = new UnicodeEncoding();
+
+        private const int CHUNK_SIZE = 128;
+
+        private void InitializeRijndael()
+        {
+            rijndael.Mode = CipherMode.CBC;
+            rijndael.Padding = PaddingMode.PKCS7;
+        }
+
+        public AES()
+        {
+            InitializeRijndael();
+
+            rijndael.Key = Encoding.UTF8.GetBytes("85c8614cfa5f497bbe7c585c86ed7e74");
+            rijndael.IV = Encoding.UTF8.GetBytes("59fff14c95754c8e");
+        }
+
+        public AES(String key, String iv)
+        {
+            InitializeRijndael();
+
+            rijndael.Key = Encoding.UTF8.GetBytes(key);
+            rijndael.IV = Encoding.UTF8.GetBytes(iv);
+        }
+
+        public AES(byte[] key, byte[] iv)
+        {
+            InitializeRijndael();
+
+            rijndael.Key = key;
+            rijndael.IV = iv;
+        }
+
+        public string Decrypt(byte[] cipher)
+        {
+            ICryptoTransform transform = rijndael.CreateDecryptor();
+            byte[] decryptedValue = transform.TransformFinalBlock(cipher, 0, cipher.Length);
+            return unicodeEncoding.GetString(decryptedValue);
+        }
+
+        public string DecryptFromBase64String(string base64cipher)
+        {
+            return Decrypt(Convert.FromBase64String(base64cipher));
+        }
+
+        public byte[] EncryptToByte(string plain)
+        {
+            ICryptoTransform encryptor = rijndael.CreateEncryptor();
+            byte[] cipher = unicodeEncoding.GetBytes(plain);
+            byte[] encryptedValue = encryptor.TransformFinalBlock(cipher, 0, cipher.Length);
+            return encryptedValue;
+        }
+
+        public string EncryptToBase64String(string plain)
+        {
+            return Convert.ToBase64String(EncryptToByte(plain));
+        }
+
+        public string GetKey()
+        {
+            return Convert.ToBase64String(rijndael.Key);
+        }
+
+        public string GetIV()
+        {
+            return Convert.ToBase64String(rijndael.IV);
+        }
+
+        public override string ToString()
+        {
+            return "KEY:" + GetKey() + Environment.NewLine + "IV:" + GetIV();
+        }
+    }
+}
