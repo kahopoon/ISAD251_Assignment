9dc08410035c3d58e80f9203731a36c3d5253c57
Johnson Kaho Poon <kahopoon@gmail.com>
2020-02-15 00:45:53 +0800

    Model Products CRUD --> commit missing 'C'

---
+++ OrderApplication/OrderApplication/Models/Products/CreateProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace OrderApplication.Models
+{
+    public class CreateProduct
+    {
+
+        public string AccessToken { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public string Price { get; set; }
+        public string Status { get; set; }
+
+    }
+
+}
