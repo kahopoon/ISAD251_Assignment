73d24a191c0c78b9f327d9ec4279aaaf64844fcb
Johnson Kaho Poon <kahopoon@gmail.com>
2020-02-15 00:48:00 +0800

    DB context

---
+++ OrderApplication/OrderApplication/Models/OrderContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderApplication.Models
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        
+        public object Tasks { get; internal set; }
+    }
+}
