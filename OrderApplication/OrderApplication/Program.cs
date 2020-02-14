c5426e8089e260fcc3eeed20aab1e431f6bfbb7d
Johnson Kaho Poon <kahopoon@gmail.com>
2020-02-15 00:38:31 +0800

    Backend ASP.NET Web API Project initialise

---
+++ OrderApplication/OrderApplication/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using System.Text;
+using OrderApplication.Models;
+
+namespace OrderApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+
+            Console.WriteLine("All done. Press any key to finish...");
+            Console.ReadKey(true);
+        
+    }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ OrderApplication/OrderApplication/Startup.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using OrderApplication.Models;
+using System.Data.SqlClient;
+
+namespace OrderApplication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            //Connection info hidden in purpose.
+            builder.DataSource = "*************.ap-northeast-1.rds.amazonaws.com";
+            builder.UserID = "********";
+            builder.Password = "**********";
+            builder.InitialCatalog = "isad251_2019_db_stud_19";
+            
+            services.AddDbContext<OrderContext>(opt =>
+               opt.UseSqlServer(builder.ConnectionString)
+            );
+            //opt.UseInMemoryDatabase("Orders"));
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
